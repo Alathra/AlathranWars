cb53a0f073664d50241de4c582e001ed49f76b92
darksaid98 <darksaid9889@gmail.com>
2024-02-29 12:52:57 +0100

    feat: in war join message

--- src/main/java/com/github/alathra/alathranwars/listeners/war/PlayerJoinListener.java
+++ src/main/java/com/github/alathra/alathranwars/listeners/war/PlayerJoinListener.java
@@ -4,12 +4,16 @@ import com.github.alathra.alathranwars.conflict.war.War;
 import com.github.alathra.alathranwars.conflict.war.WarController;
 import com.github.alathra.alathranwars.conflict.war.side.Side;
 import com.github.alathra.alathranwars.hooks.NameColorHandler;
+import com.github.milkdrinkers.colorparser.ColorParser;
+import net.kyori.adventure.title.Title;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+
+import java.time.Duration;
 
 public class PlayerJoinListener implements Listener {
     @EventHandler
@@ -26,5 +30,15 @@ public class PlayerJoinListener implements Listener {
             }
         }
         NameColorHandler.getInstance().calculatePlayerColors(p);
+
+        if (WarController.getInstance().isPlayerInAnyWars(p)) {
+            p.showTitle(
+                Title.title(
+                    ColorParser.of("<gradient:#D72A09:#B01F03><u><b>War").build(),
+                    ColorParser.of("<gray><i>You are in at least one war!").build(),
+                    Title.Times.times(Duration.ofMillis(500), Duration.ofMillis(3500), Duration.ofMillis(500))
+                )
+            );
+        }
     }
 }
