c9261ea48028073601c4002a99e6d13297c172e4
darksaid98 <darksaid9889@gmail.com>
2024-09-07 16:36:17 +0200

    feat: block head drop during war

--- src/main/java/com/github/alathra/alathranwars/listeners/siege/PlayerHeadsPlusListener.java
+++ src/main/java/com/github/alathra/alathranwars/listeners/siege/PlayerHeadsPlusListener.java
@@ -1,6 +1,8 @@
 package com.github.alathra.alathranwars.listeners.siege;
 
+import com.github.alathra.alathranwars.AlathranWars;
 import com.github.alathra.alathranwars.conflict.battle.siege.Siege;
+import com.github.alathra.alathranwars.conflict.war.WarController;
 import com.github.alathra.alathranwars.utility.Utils;
 import io.github.thatsmusic99.headsplus.api.events.PlayerHeadDropEvent;
 import org.bukkit.event.EventHandler;
@@ -9,6 +11,11 @@ import org.bukkit.event.Listener;
 public class PlayerHeadsPlusListener implements Listener {
     @EventHandler
     public void onHeadDrop(PlayerHeadDropEvent e) {
+        if (AlathranWars.getInstance().isWarTime() && WarController.getInstance().isInAnyWars(e.getDeadPlayer())) {
+            e.setCancelled(true);
+            return;
+        }
+
         Siege siege = Utils.getClosestSiege(e.getDeadPlayer(), false);
         if (siege == null) return;
 
