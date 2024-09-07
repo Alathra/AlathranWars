2e6d4d897fcdf6d725d9c50b63cb52ed42035d94
darksaid98 <darksaid9889@gmail.com>
2024-09-07 16:41:11 +0200

    feat: prevent grave creation during wartime

--- src/main/java/com/github/alathra/alathranwars/listeners/siege/PlayerGraveListener.java
+++ src/main/java/com/github/alathra/alathranwars/listeners/siege/PlayerGraveListener.java
@@ -1,6 +1,8 @@
 package com.github.alathra.alathranwars.listeners.siege;
 
+import com.github.alathra.alathranwars.AlathranWars;
 import com.github.alathra.alathranwars.conflict.battle.siege.Siege;
+import com.github.alathra.alathranwars.conflict.war.WarController;
 import com.github.alathra.alathranwars.utility.Utils;
 import com.ranull.graves.event.GraveCreateEvent;
 import org.bukkit.entity.EntityType;
@@ -14,6 +16,11 @@ public class PlayerGraveListener implements Listener {
     public void onGraveCreate(GraveCreateEvent e) {
         if (!e.getEntityType().equals(EntityType.PLAYER)) return;
         if (!(e.getEntity() instanceof Player p)) return;
+
+        if (AlathranWars.getInstance().isWarTime() && WarController.getInstance().isInAnyWars(p)) {
+            e.setCancelled(true);
+            return;
+        }
 
         Siege siege = Utils.getClosestSiege(p, false);
         if (siege == null) return;
